94b27538f399841b6bfe5fa8ff3e6f6b4992d6f2
Naoya Yamashita <conao3@gmail.com>
2022-02-20 14:03:35 +0900

    move function

--- src/c3tools/lib/subr.py
+++ src/c3tools/lib/subr.py
@@ -5,17 +5,17 @@ def identity(elm: Any) -> Any:
     return elm
 
 
-def keep_if(pred: Callable[[Any], bool], seq: list[Any]) -> list[Any]:
-    return [elm for elm in seq if pred(elm)]
-
-
-def keep(seq: list[Any]):
-    return keep_if(bool, seq)
-
-
 def remove_if(pred: Callable[[Any], bool], seq: list[Any]) -> list[Any]:
     return [elm for elm in seq if not pred(elm)]
 
 
+def keep_if(pred: Callable[[Any], bool], seq: list[Any]) -> list[Any]:
+    return [elm for elm in seq if pred(elm)]
+
+
 def remove(elt: Any, seq: list[Any]) -> list[Any]:
     return remove_if(lambda elm: elm == elt, seq)
+
+
+def keep(seq: list[Any]):
+    return keep_if(bool, seq)
