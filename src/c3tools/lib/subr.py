9e819081ee52ec376518328ccf32d5b6c3adaf9b
Naoya Yamashita <conao3@gmail.com>
2022-02-20 14:02:02 +0900

    implement remove, remove_if

--- src/c3tools/lib/subr.py
+++ src/c3tools/lib/subr.py
@@ -11,3 +11,11 @@ def keep_if(pred: Callable[[Any], bool], args: list[Any]) -> list[Any]:
 
 def keep(args: list[Any]):
     return keep_if(bool, args)
+
+
+def remove_if(pred: Callable[[Any], bool], seq: list[Any]) -> list[Any]:
+    return [elm for elm in seq if not pred(elm)]
+
+
+def remove(elt: Any, seq: list[Any]) -> list[Any]:
+    return remove_if(lambda elm: elm == elt, seq)
